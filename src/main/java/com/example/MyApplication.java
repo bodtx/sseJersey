6b63d5c8372064c1c6aa651653a23edf6a193542
Alexis Krier <alexis.krier@laposte.net>
2014-01-19 21:57:40 +0100

    init

---
+++ src/main/java/com/example/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.media.sse.SseFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("resources")
+public class MyApplication extends ResourceConfig {
+    public MyApplication() {
+    	super.registerClasses(SseFeature.class,MyResource.class,MyResourceSSE.class);
+//        packages("com.example");
+    }
+}
---
+++ src/main/java/com/example/MyJaxbBean.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class MyJaxbBean {
+    public String name;
+    public int age;
+ 
+    public MyJaxbBean() {} // JAXB needs this
+ 
+    public MyJaxbBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
