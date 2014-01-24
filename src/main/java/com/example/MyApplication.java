5e1c10f854152fd58e718b4d5719b37bd3f891a9
bodtx <alexis.krier@laposte.net>
2014-01-24 12:55:54 +0100

    Broadcaster avec injection HK2

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -2,13 +2,19 @@ package com.example;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("resources")
 public class MyApplication extends ResourceConfig {
     public MyApplication() {
-    	super.registerClasses(SseFeature.class,MyResource.class,MyResourceSSE.class, MyBroadcasterSSE.class);
-//        packages("com.example");
+        super.registerClasses(SseFeature.class, MyBroadcasterSSE.class);
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(SerialTest.class).to(SerialTest.class);
+            }
+        });
     }
 }
--- src/main/java/com/example/MyBroadcasterSSE.java
+++ src/main/java/com/example/MyBroadcasterSSE.java
@@ -1,17 +1,13 @@
 package com.example;
 
-import java.io.IOException;
-
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.media.sse.EventOutput;
-import org.glassfish.jersey.media.sse.OutboundEvent;
-import org.glassfish.jersey.media.sse.SseBroadcaster;
 import org.glassfish.jersey.media.sse.SseFeature;
 
 /**
@@ -20,45 +16,33 @@ import org.glassfish.jersey.media.sse.SseFeature;
 @Singleton
 @Path("broadcast")
 public class MyBroadcasterSSE {
-	private SseBroadcaster broadcaster = new SseBroadcaster();
-	
-	@PostConstruct
-	void init () {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-				    try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				    broadcastMessage(new MyJaxbBean("Alexis",i));
-				}
-            }
-        }).start();
+
+    @Inject
+    SerialTest serialTest;
+
+    @PostConstruct
+    void init() throws Exception {
+        // new Thread(new Runnable() {
+        // @Override
+        // public void run() {
+        // for (int i = 0; i < 100; i++) {
+        // try {
+        // Thread.sleep(1000);
+        // } catch (InterruptedException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        // // broadcastMessage(SerialTest.infoBureau);
+        // }
+        // }
+        // }).start();
     }
-	
-	
-	private  void broadcastMessage(MyJaxbBean message) {
-        OutboundEvent.Builder eventBuilder = new OutboundEvent.Builder();
-        OutboundEvent event = eventBuilder
-            .mediaType(MediaType.APPLICATION_JSON_TYPE)
-            .data(MyJaxbBean.class, message)
-            .build();
- 
-        broadcaster.broadcast(event);
- 
-        System.out.println( "Message was '" + message + "' broadcast.");
-    }
-	
-	@GET
+
+    @GET
     @Produces(SseFeature.SERVER_SENT_EVENTS)
     public EventOutput listenToBroadcast() {
         final EventOutput eventOutput = new EventOutput();
-        this.broadcaster.add(eventOutput);
+        serialTest.broadcaster.add(eventOutput);
         return eventOutput;
     }
 }
-
